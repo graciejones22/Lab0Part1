606a3a143164a787a07fd0acda3115dcd137a9d4
graciejones22 <gp09jone@siena.edu>
2026-09-14 08:48:13 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,15 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Gracie Jones
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("I'm from Hudson, WI!");
+        System.out.println("My favorite restaurant is Melt.");
+        System.out.println("I like to order the Guy Fiery gf pizza.");
     }
 }
